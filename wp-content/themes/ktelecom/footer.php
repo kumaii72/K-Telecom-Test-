e48897ba3cf93da8cfe82240e181a1696da60a2f
kumaii72 <kumaii@ya.ru>
2024-06-23 22:37:20 +0500

    css+slider

--- wp-content/themes/ktelecom/footer.php
+++ wp-content/themes/ktelecom/footer.php
@@ -1,7 +1,47 @@
 <footer>
+    <div>
 <?php echo get_custom_logo(); ?>
+</div>
+<div class="company">© 2018–2022 ООО «К-Телеком»</div>
+<div class="social">
+    <a href=#>
+        <img src="wp-content\themes\ktelecom\assets\image\1.png">
+    </a>
+    <a href=#>
+        <img src="wp-content\themes\ktelecom\assets\image\2.png">
+    </a>
+    <a href=#>
+        <img src="wp-content\themes\ktelecom\assets\image\3.png">
+    </a>
+</div>
+
 </footer>
 </div> <!-- .wrapper -->
 <?php wp_footer(); ?>
+<script src="wp-content\themes\ktelecom\assets\owlcarousel\jquery-3.7.1.min.js"></script>
+<script src="wp-content\themes\ktelecom\assets\owlcarousel\owl.carousel.min.js"></script>
+<script>
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoWidth:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
+</script>
 </body>
 </html> 
--- wp-content/themes/ktelecom/header.php
+++ wp-content/themes/ktelecom/header.php
@@ -3,6 +3,9 @@
 <head>
 	<title>К-Телеком - тестовое задание</title>
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+    <link rel="stylesheet" href="wp-content\themes\ktelecom\assets\owlcarousel\assets\owl.carousel.min.css">
+<link rel="stylesheet" href="wp-content\themes\ktelecom\assets\owlcarousel\assets\owlcarousel\owl.theme.default.min.css">
+
     <?php wp_head() ?>
 </head>
 
