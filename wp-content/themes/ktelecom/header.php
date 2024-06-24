3d0c467f5a1f9702e88c252a31bac43c7e182b2b
kumaii72 <kumaii@ya.ru>
2024-06-24 12:39:40 +0500

    media query

--- wp-content/themes/ktelecom/header.php
+++ wp-content/themes/ktelecom/header.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>К-Телеком - тестовое задание</title>
-	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
-    <link rel="stylesheet" href="wp-content\themes\ktelecom\assets\owlcarousel\assets\owl.carousel.min.css">
-<link rel="stylesheet" href="wp-content\themes\ktelecom\assets\owlcarousel\assets\owlcarousel\owl.theme.default.min.css">
 
+<head>
+    <title>К-Телеком - тестовое задание</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+    <link rel="stylesheet" href="wp-content\themes\ktelecom\assets\css\media.css" type="text/css" />
+    <link rel="stylesheet" href="wp-content\themes\ktelecom\assets\owlcarousel\assets\owl.carousel.min.css"
+        type="text/css" />
+    <link rel="stylesheet" href="wp-content\themes\ktelecom\assets\owlcarousel\assets\owl.theme.default.min.css"
+        type="text/css" />
     <?php wp_head() ?>
 </head>
 
 <body>
     <div class="wrapper">
 
-<header>
-    <?php echo get_custom_logo(); ?>
-    </header>
+        <header>
+            <?php echo get_custom_logo(); ?>
+        </header>
--- wp-content/themes/ktelecom/index.php
+++ wp-content/themes/ktelecom/index.php
@@ -1,10 +1,11 @@
-<?php get_header(); 
-if (have_posts()) : 
-    while (have_posts()) : the_post();
-        the_title('<h2>', '</h2>'); 
-        the_content(); 
+<?php get_header();
+if (have_posts()):
+    while (have_posts()):
+        the_post();
+        the_title('<h2>', '</h2>');
+        the_content();
     endwhile;
-else :
+else:
     echo '<p>Записи не найдены</p>';
 endif;
 get_footer(); ?>
